8623ddc6ff0e992a5a2e26d19e0264751714e265
Przemysław Wróbel <przemek.wrobel@poczta.fm>
2018-05-03 23:32:31 +0200

    First

---
+++ script-compiled.js
@@ -0,0 +1,136 @@
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var timeTable = [];
+
+var startButton = document.getElementById('start');
+startButton.addEventListener('click', function () {
+    return stopwatch.start();
+});
+
+var stopButton = document.getElementById('stop');
+stopButton.addEventListener('click', function () {
+    return stopwatch.stop();
+});
+/*
+let resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', () => {stopwatch.reset(), stopwatch.print()} );
+/*
+let addButton = document.getElementById('add');
+addButton.addEventListener('click', () => {stopwatch.get(), stopwatch.results()});
+
+let clearButton = document.getElementById('clear');
+clearButton.addEventListener('click', () => stopwatch.clear());
+*/
+
+var Stopwatch = function () {
+    function Stopwatch(display) {
+        _classCallCheck(this, Stopwatch);
+
+        this.running = false;
+        this.display = display;
+        this.reset();
+        this.print(this.times);
+    }
+
+    _createClass(Stopwatch, [{
+        key: 'reset',
+        value: function reset() {
+            this.times = {
+                minutes: 0,
+                seconds: 0,
+                miliseconds: 0
+            };
+        }
+    }, {
+        key: 'print',
+        value: function print() {
+            this.display.innerText = this.format(this.times);
+        }
+    }, {
+        key: 'format',
+        value: function format(times) {
+            return pad0(times.minutes) + ':' + pad0(times.seconds) + ':' + pad0(Math.floor(times.miliseconds));
+        }
+    }, {
+        key: 'start',
+        value: function start() {
+            var _this = this;
+
+            if (!this.running) {
+                this.running = true;
+                this.watch = setInterval(function () {
+                    return _this.step();
+                }, 10);
+            }
+        }
+    }, {
+        key: 'step',
+        value: function step() {
+            if (!this.running) return;
+            this.calculate();
+            this.print();
+        }
+    }, {
+        key: 'calculate',
+        value: function calculate() {
+            this.times.miliseconds += 1;
+            if (this.times.miliseconds >= 100) {
+                this.times.seconds += 1;
+                this.times.miliseconds = 0;
+            }
+            if (this.times.seconds >= 60) {
+                this.times.minutes += 1;
+                this.times.seconds = 0;
+            }
+        }
+    }, {
+        key: 'stop',
+        value: function stop() {
+            this.running = false;
+            clearInterval(this.watch);
+        }
+    }, {
+        key: 'get',
+        value: function get() {
+            console.log(this.format(this.times));
+            timeTable = [].concat(_toConsumableArray(timeTable), [this.format(this.times)]);
+            console.log(timeTable);
+        }
+    }, {
+        key: 'clear',
+        value: function clear() {
+            timeTable = [];
+            console.log(timeTable);
+            document.getElementById('results').innerHTML = "";
+        }
+    }, {
+        key: 'results',
+        value: function results() {
+            var pos = timeTable.length - 1;
+            this.val = timeTable;
+            var addTime = timeTable[pos];
+            var res = document.querySelector('.results');
+            var list = document.createElement("li");
+            list.innerText = addTime;
+            res.appendChild(list);
+        }
+    }]);
+
+    return Stopwatch;
+}();
+
+function pad0(value) {
+    var result = value.toString();
+    if (result.length < 2) {
+        result = '0' + result;
+    }
+    return result;
+}
+
+var stopwatch = new Stopwatch(document.querySelector('.stopwatch'));
